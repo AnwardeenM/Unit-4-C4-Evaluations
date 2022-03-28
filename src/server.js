516b6d1774330fc22d74da958569dabfd1b972b9
AnwardeenM <96279461+AnwardeenM@users.noreply.github.com>
2022-03-28 10:11:17 +0530

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const app = require("./index");
+
+// const connect = require("./configs/database")
+
+const user = require("./models/User.model");
+
+const todo = require("./models/Todo.model") 
+
+// create POST request that the user can register 
+
+//create a GET request for the user to login 
+
+
+//create a POST("/todos") for the users to get the overall todo list 
+
+//create GET request 
+
